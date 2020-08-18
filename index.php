1e46bd5efd0064608599c0a4d116e9b19b8b6740
Christian-777 <Christian.c.g.f777@hotmail.com>
2020-08-18 20:06:50 -0300

    20200811

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<!DOCTYPE HTML>
-<html lang="pt-br">
-	<head>
-		<meta charset="UTF-8"/>
-		<title>Organizacao</title>
-	</head>
-	<body>
-		<p><h1>Pagina com os links das minhas atividades</h1></p>
-	</body>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-sacle=1">
+        <title>Inicio</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Página com links das minhas atividades</h1>
+            <p> <a href="20200811/index.php">Link Exercicio Modificação de Texto</a></p>
+        </div>
+    </body>
 </html>
-
